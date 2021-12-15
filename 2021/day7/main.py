12f763d69aa2e3d910f7bcb97d7fc5a7491524ca
Sayre Blades <sayre.blades@gmail.com>
2021-12-14 22:46:06 -0500

    reorg

--- 2021/day7/main.py
+++ 2021/day7/main.py
@@ -1,10 +1,9 @@
 import os
 
+import numpy as np
 import tqdm
 import dask
-from dask.distributed import Client
-from dask.distributed import progress
-from dask.distributed import LocalCluster
+from numba import jit
 
 
 HERE = os.path.dirname(__file__)
@@ -15,7 +14,7 @@ DATA_FILE = os.path.join(HERE, 'input.txt')
 def get_data(fname):
     with open(fname, 'r') as f:
         data = "".join(f.readlines())
-        return [int(d) for d in data.split(',')]
+        return np.array([int(d) for d in data.split(',')])
 
 
 def part1(fname):
@@ -29,23 +28,21 @@ def part1(fname):
     return min(results.values())
 
 
+@jit(nopython=True)
+def process(data, i):
+    sum = 0
+    for d in data:
+        for f in range(1, abs(d - i)+1):
+            sum += f
+    return sum
+
 
 def part2(fname):
     data = get_data(fname)
-
-    def process(i):
-        sum = 0
-        for d in data:
-            for f in range(1, abs(d - i)+1):
-                sum += f
-        return sum
-
-    cluster = LocalCluster(n_workers=8)
-    client = Client(cluster)
-    print(f"\ncheck job status at {client.dashboard_link}\n")
-    futs = client.map(process, range(min(data), max(data)))
-    progress(futs)
-    return min(client.gather(futs))
+    results = []
+    for i in tqdm.tqdm(range(min(data), max(data))):
+        results.append(process(data, i))
+    return min(results)
 
 
 if __name__ == "__main__":
@@ -53,4 +50,3 @@ if __name__ == "__main__":
     print(part1(DATA_FILE))  # 364898
     print(part2(TEST_FILE))  # 168
     print(part2(DATA_FILE))  # 104149091
-    input()
